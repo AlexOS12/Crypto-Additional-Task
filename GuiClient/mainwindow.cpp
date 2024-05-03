f7e9e549e2c91b27775ffc6e7d93d8787d844d17
AlexOS12 <sluhach2016@gmail.com>
2024-05-03 21:02:53 +0300

    Создал базовую прогу, которая умеет чекать права админа

---
+++ GuiClient/mainwindow.cpp
@@ -0,0 +1,70 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    if (IsAppRunningAsAdminMode()) {
+        ui->label->setText("ADMIN :)");
+    } else {
+        ui->label->setText(("NOT ADMIN :("));
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+WINBOOL MainWindow::IsAppRunningAsAdminMode()
+{
+    BOOL fIsRunAsAdmin = FALSE;
+    DWORD dwError = ERROR_SUCCESS;
+    PSID pAdministratorsGroup = NULL;
+
+    // Allocate and initialize a SID of the administrators group.
+
+    SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+    if (!AllocateAndInitializeSid(
+            &NtAuthority,
+            2,
+            SECURITY_BUILTIN_DOMAIN_RID,
+            DOMAIN_ALIAS_RID_ADMINS,
+            0, 0, 0, 0, 0, 0,
+            &pAdministratorsGroup))
+    {
+        dwError = GetLastError();
+        goto Cleanup;
+    }
+
+    // Determine whether the SID of administrators group is enabled in
+
+    // the primary access token of the process.
+
+    if (!CheckTokenMembership(NULL, pAdministratorsGroup, &fIsRunAsAdmin))
+    {
+        dwError = GetLastError();
+        goto Cleanup;
+    }
+
+Cleanup:
+    // Centralized cleanup for all allocated resources.
+
+    if (pAdministratorsGroup)
+    {
+        FreeSid(pAdministratorsGroup);
+        pAdministratorsGroup = NULL;
+    }
+
+    // Throw the error if something failed in the function.
+
+    if (ERROR_SUCCESS != dwError)
+    {
+        throw dwError;
+    }
+
+    return fIsRunAsAdmin;
+}
